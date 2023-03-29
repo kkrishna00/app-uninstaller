d6d10332738e267de43f06ac3f996079a47fad85
kkrishna00 <krishnakishu9@gmail.com>
2023-03-29 20:21:24 +0530

    code : remove unused code

--- app/src/main/java/com/example/appunistaller/MainActivity.kt
+++ app/src/main/java/com/example/appunistaller/MainActivity.kt
@@ -86,34 +86,7 @@ class MainActivity : AppCompatActivity() {
             userApps = userPack
         )
     }
-
-    private fun drawableToBitmap(drawable: Drawable): Bitmap? {
-        var bitmap: Bitmap? = null
-        if (drawable is BitmapDrawable) {
-            if (drawable.bitmap != null) {
-                return drawable.bitmap
-            }
-        }
-        bitmap = if (drawable.intrinsicWidth <= 0 || drawable.intrinsicHeight <= 0) {
-            Bitmap.createBitmap(
-                1,
-                1,
-                Bitmap.Config.ARGB_8888
-            ) // Single color bitmap will be created of 1x1 pixel
-        } else {
-            Bitmap.createBitmap(
-                drawable.intrinsicWidth,
-                drawable.intrinsicHeight,
-                Bitmap.Config.ARGB_8888
-            )
-        }
-        val canvas = Canvas(bitmap)
-        drawable.setBounds(0, 0, canvas.width, canvas.height)
-        drawable.draw(canvas)
-        return bitmap
-    }
 }
-
 
 object MemoryStatus {
     private const val ERROR = -1
