378001807423a0b756d8e3fec3ad75fe74d174bc
kkrishna00 <krishnakishu9@gmail.com>
2023-03-29 14:54:54 +0530

    code : added storage info

--- app/src/main/java/com/example/appunistaller/MainActivity.kt
+++ app/src/main/java/com/example/appunistaller/MainActivity.kt
@@ -4,10 +4,14 @@ import android.content.pm.ApplicationInfo
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Environment
+import android.os.StatFs
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.appunistaller.MemoryStatus.bytesToHuman
 import com.example.appunistaller.databinding.ActivityMainBinding
+import java.text.DecimalFormat
 
 
 class MainActivity : AppCompatActivity(), MainActivityController {
@@ -19,6 +23,17 @@ class MainActivity : AppCompatActivity(), MainActivityController {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupRv()
+        setupMemoryStatus()
+    }
+
+    private fun setupMemoryStatus() {
+        binding.memoryStatus.text =
+            buildString {
+                append("Free Memory: ")
+                append(MemoryStatus.availableInternalMemorySize.bytesToHuman())
+                append(" / ")
+                append(MemoryStatus.totalInternalMemorySize.bytesToHuman())
+            }
     }
 
     private fun setupRv() {
@@ -57,5 +72,76 @@ class MainActivity : AppCompatActivity(), MainActivityController {
 
     override fun handleActionButton(packageInfo: PackageInfo) {
         AppActionContainerActivity.startActivity(this, screenData = ScreenData(packageInfo))
+    }
+}
+
+
+object MemoryStatus {
+    private const val ERROR = -1
+    private fun externalMemoryAvailable(): Boolean {
+        return Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
+    }
+
+    val availableInternalMemorySize: Long
+        get() {
+            val path = Environment.getDataDirectory()
+            val stat = StatFs(path.path)
+            val blockSize = stat.blockSize.toLong()
+            val availableBlocks = stat.availableBlocks.toLong()
+            return availableBlocks * blockSize
+        }
+
+    val totalInternalMemorySize: Long
+        get() {
+            val path = Environment.getDataDirectory()
+            val stat = StatFs(path.path)
+            val blockSize = stat.blockSize.toLong()
+            val totalBlocks = stat.blockCount.toLong()
+            return totalBlocks * blockSize
+        }
+
+    val availableExternalMemorySize: Long
+        get() = if (externalMemoryAvailable()) {
+            val path = Environment.getExternalStorageDirectory()
+            val stat = StatFs(path.path)
+            val blockSize = stat.blockSize.toLong()
+            val availableBlocks = stat.availableBlocks.toLong()
+            availableBlocks * blockSize
+        } else {
+            ERROR.toLong()
+        }
+
+    val totalExternalMemorySize: Long
+        get() {
+            return if (externalMemoryAvailable()) {
+                val path = Environment.getExternalStorageDirectory()
+                val stat = StatFs(path.path)
+                val blockSize = stat.blockSize.toLong()
+                val totalBlocks = stat.blockCount.toLong()
+                totalBlocks * blockSize
+            } else {
+                ERROR.toLong()
+            }
+        }
+
+    private fun floatForm(d: Double): String {
+        return DecimalFormat("#.##").format(d)
+    }
+
+    fun Long.bytesToHuman(): String {
+        val size = this
+        val kb = (1 * 1024).toLong()
+        val mb = kb * 1024
+        val gb = mb * 1024
+        val tb = gb * 1024
+        val pb = tb * 1024
+        val eb = pb * 1024
+        if (size < kb) return floatForm(size.toDouble()) + " byte"
+        if (size in kb until mb) return floatForm(size.toDouble() / kb) + " KB"
+        if (size in mb until gb) return floatForm(size.toDouble() / mb) + " MB"
+        if (size in gb until tb) return floatForm(size.toDouble() / gb) + " GB"
+        if (size in tb until pb) return floatForm(size.toDouble() / tb) + " TB"
+        if (size in pb until eb) return floatForm(size.toDouble() / pb) + " PB"
+        return floatForm(size.toDouble() / eb) + " EB"
     }
 }
