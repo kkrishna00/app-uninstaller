9215c61ea7d4c09b3c7c1147b3ab3e4e9dbe64ce
kkrishna00 <krishnakishu9@gmail.com>
2023-04-06 00:55:32 +0530

    code : custom splash screen

---
+++ app/src/main/java/com/example/appunistaller/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.appunistaller.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.example.appunistaller.R
+import com.example.appunistaller.activity.MainActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        lifecycleScope.launch {
+            delay(3000L)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }
+    }
+}
