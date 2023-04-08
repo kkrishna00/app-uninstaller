018f85e6b4699989c194e9e05a95a3d160d3c7a2
kkrishna00 <krishnakishu9@gmail.com>
2023-04-09 00:33:00 +0530

    code : add filtering

--- app/src/main/java/com/example/appunistaller/activity/MainActivity.kt
+++ app/src/main/java/com/example/appunistaller/activity/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.appunistaller.activity
 
+import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.appunistaller.Utils.MemoryStatus
-import com.example.appunistaller.viewPager.DemoCollectionPagerAdapter
-import com.example.appunistaller.pojo.PackageInfoContainer
-import com.example.appunistaller.fragment.ViewPagerAdapterScreenData
 import com.example.appunistaller.Utils.MemoryStatus.bytesToHuman
 import com.example.appunistaller.databinding.ActivityMainBinding
+import com.example.appunistaller.fragment.ViewPagerAdapterScreenData
+import com.example.appunistaller.pojo.PackageInfoContainer
+import com.example.appunistaller.viewPager.DemoCollectionPagerAdapter
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.io.File
 
 
@@ -24,7 +32,58 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupRv()
+        supportActionBar?.hide()
+        setSupportActionBar(binding.toolbar)
         setupMemoryStatus()
+        setupSearch()
+    }
+
+    private fun setupSearch() {
+        binding.searchIcon.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                lifecycleScope.launch {
+                    delay(300L)
+                    if (query.length >= 3) {
+                        filterList(query.lowercase())
+                    } else if (query.isEmpty()) {
+                        setupRv()
+                    }
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                lifecycleScope.launch {
+                    delay(300L)
+                    if (newText.length >= 3) {
+                        filterList(newText.lowercase())
+                    } else if(newText.isEmpty()) {
+                        setupRv()
+                    }
+                }
+                return false
+            }
+        })
+    }
+
+    private fun hideSoftKeyboard(view: View) {
+        val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun filterList(query: String) {
+        val list = getInstalledApps()
+        val filteredList = list.copy(
+            userApps = list.userApps.filter { packageInfoContainer ->
+                packageInfoContainer.name?.lowercase()?.contains(query) == true
+            },
+            systemApps = list.systemApps.filter { packageInfoContainer ->
+                packageInfoContainer.name?.lowercase()?.contains(query) == true
+            }
+        )
+        Log.d("KRISHNA", filteredList.toString())
+        (homeAdapter as DemoCollectionPagerAdapter).updateData(filteredList)
     }
 
     private fun setupMemoryStatus() {
